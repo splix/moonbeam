f7997badfbb87698fcfb59f9c856cf0f37fe21fb
Igor Artamonov <igor@artamonov.ru>
2020-02-11 21:56:18 -0500

    solution: extract address type

--- src/main/kotlin/io/emeraldpay/polkadotcrawler/state/ProcessedPeerDetails.kt
+++ src/main/kotlin/io/emeraldpay/polkadotcrawler/state/ProcessedPeerDetails.kt
@@ -15,16 +15,21 @@ open class ProcessedPeerDetails(
             val address: String,
             val hostname: String?,
             val ip: String?,
-            val port: Int?
+            val port: Int?,
+            val type: String?
     ) {
         companion object {
             fun from(peer: PeerDetails): Host {
+                var type: String? = null
                 val ip = if (peer.address.has(Protocol.IP4)) {
+                    type = "IP"
                     peer.address.getStringComponent(Protocol.IP4)
                 } else if (peer.address.has(Protocol.IP6)) {
+                    type = "IP"
                     peer.address.getStringComponent(Protocol.IP6)
                 } else {
                     peer.address.getStringComponent(Protocol.DNS4)?.let { host ->
+                        type = "DNS"
                         InetAddress.getByName(host).hostAddress
                     }
                 }
@@ -33,7 +38,8 @@ open class ProcessedPeerDetails(
                         peer.address.toString(),
                         peer.address.getStringComponent(Protocol.DNS4),
                         ip,
-                        port
+                        port,
+                        type
                 )
             }
         }
